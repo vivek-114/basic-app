3df796a44493af8a5210a7de5aa5fb43471b04f6
unknown <vivekvelugubantla114@gmail.com>
2022-12-18 15:22:00 +0530

    Adding state functionality for the registration component.

--- trail_app/src/component/Registration.js
+++ trail_app/src/component/Registration.js
@@ -3,6 +3,19 @@ import React from "react";
 class Registration extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            email: "",
+            password: "",
+            password_confirmation: ""
+        }
+    }
+
+    createUser = (event) => {
+        const {
+            email,
+            password,
+            password_confirmation
+        } = this.state
     }
     render(){
         return(
@@ -10,15 +23,17 @@ class Registration extends React.Component{
                 <div>
                     <label> Email: </label>
                     <br></br>
-                    <input type="text" name="email" placeholder="Email"/>
+                    <input type="text" name="email" placeholder="Email" value = {this.state.email}/>
                     <br></br>
                     <label> Password:</label>
                     <br></br>
-                    <input type="text" name="password" placeholder="Password"/>
+                    <input type="text" name="password" placeholder="Password" value = {this.state.password}/>
                     <br></br>
                     <label> Password Confirmation:</label>
                     <br></br>
-                    <input type="text" name="password_confirmation" placeholder="Password Confirmation"/>
+                    <input type="text" name="password_confirmation" placeholder="Password Confirmation" value = {this.state.password_confirmation}/>
+                    <br></br>
+                    <button type="submit">Create User</button>
                 </div>
             </form>
         );
